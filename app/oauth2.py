fcb9060dd629555e21a7505368d9a67f02b54077
Juan Cruz Otero <juancoterom@gmail.com>
2023-04-18 17:10:48 -0300

    Arreglos en configuración.

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,4 +1,5 @@
 from . import schemas, database, models
+from .config import settings
 from fastapi import status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
@@ -9,9 +10,9 @@ from sqlalchemy.orm import Session
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 
 def create_access_token(data: dict):
