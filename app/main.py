6be1dee89ec1144f89413b49691fc1aeddfc2c7b
Juan Cruz Otero <juancoterom@gmail.com>
2023-04-18 01:38:54 -0300

    Implementación de get_one_user.

--- app/main.py
+++ app/main.py
@@ -1,15 +1,9 @@
-import psycopg2
-import time
 from fastapi import FastAPI, Response, status, HTTPException, Depends
-from fastapi.params import Body
-from pydantic import BaseModel
-from typing import Optional, List
-from random import randrange
-from psycopg2.extras import RealDictCursor
-from . import models, scheemas
 from .database import engine, get_db
+from . import models, schemas, utils
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
+from typing import List
 
 
 models.Base.metadata.create_all(bind=engine)
@@ -21,7 +15,7 @@ def root():
     return {"detail": "Hello world."}
 
 
-@app.get("/posts", response_model=List[scheemas.PostResponse])
+@app.get("/posts", response_model=List[schemas.PostResponse])
 def get_posts(db: Session = Depends(get_db)):
     """ Retrieves all posts from the database. """
 
@@ -30,7 +24,7 @@ def get_posts(db: Session = Depends(get_db)):
     return posts
 
 
-@app.get("/posts/{id}", response_model=scheemas.PostResponse)
+@app.get("/posts/{id}", response_model=schemas.PostResponse)
 def get_one_post(id: int, db: Session = Depends(get_db)):
     """ Retrieves a single post from the database, given a post id. """
 
@@ -45,8 +39,8 @@ def get_one_post(id: int, db: Session = Depends(get_db)):
     return post
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=scheemas.PostResponse)
-def create_post(post: scheemas.PostCreate, db: Session = Depends(get_db)):
+@app.post("/posts", status_code=status.HTTP_201_CREATED, response_model=schemas.PostResponse)
+def create_post(post: schemas.PostCreate, db: Session = Depends(get_db)):
     """ Writes a new entry into the database, given a post. """
 
     new_post = models.Post(**post.dict())
@@ -75,8 +69,8 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@app.put("/posts/{id}", response_model=scheemas.PostResponse)
-def update_post(id: int, updated_post: scheemas.PostCreate, db: Session = Depends(get_db)):
+@app.put("/posts/{id}", response_model=schemas.PostResponse)
+def update_post(id: int, updated_post: schemas.PostCreate, db: Session = Depends(get_db)):
     """ Updates an entry from the database, given a post id and an updated post. """
 
     post_query = db.query(models.Post).filter(models.Post.id == id)
@@ -93,20 +87,40 @@ def update_post(id: int, updated_post: scheemas.PostCreate, db: Session = Depend
 
     return post_query.first()
 
-@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=scheemas.UserResponse)
-def create_user(user: scheemas.UserCreate, db: Session = Depends(get_db)):
+
+@app.get("/users/{id}", response_model=schemas.UserResponse)
+def get_one_user(id: int, db: Session = Depends(get_db)):
+    """ Retrieves a single user from the database, given a user id. """
+
+    user = db.query(models.User).filter(models.User.id == id).first()
+
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found."
+            )
+    
+    return user
+
+
+@app.post("/users", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     """ Adds a new user into the database, given the email and password. """
     
+    # Hash password.
+    user.password = utils.hash(user.password)
+
     new_user = models.User(**user.dict())
     db.add(new_user)
-    
+
+    # Check if email is available.
     try:
         db.commit()
 
     except SQLAlchemyError:
         raise HTTPException(
             status_code=status.HTTP_200_OK,
-            detail="Email already in use."
+            detail="Email is already in use."
             )
     
     db.refresh(new_user)
