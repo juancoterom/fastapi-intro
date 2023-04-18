a1680949a6f1d8cf47fdeb84c3a9945084d980df
Juan Cruz Otero <juancoterom@gmail.com>
2023-04-17 21:46:15 -0300

    Implementación de métodos get/post/delete/push con raw SQL.

--- app/main.py
+++ app/main.py
@@ -17,6 +17,7 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+
 while True:
     try:
         conn = psycopg2.connect(
@@ -28,37 +29,12 @@ while True:
             )
         cursor = conn.cursor()
         print("Database connection was succesful.")
+        break
 
     except Exception as error:
         print("Connection to database failed.")
         print("Error: ", error)
         time.sleep(2)
-
-
-my_posts = [
-    {
-        "title": "Title of post 1",
-        "content": "Content of post 1.",
-        "id": 1
-    },
-    {
-        "title": "Title of post 2",
-        "content": "I like pizza.",
-        "id": 2
-    }
-]
-
-
-def find_post(id):
-    for p in my_posts:
-        if p["id"] == id:
-            return p
-
-
-def find_index_post(id):
-    for i, p in enumerate(my_posts):
-        if p['id'] == id:
-            return i
 
 
 @app.get("/")
@@ -68,18 +44,15 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"detail": my_posts}
-
-
-@app.get("/posts/latest")
-def get_latest_post():
-    post = my_posts[len(my_posts)-1]
-    return {"detail": post}
+    cursor.execute("""SELECT * FROM posts """)
+    posts = cursor.fetchall()
+    return {"detail": posts}
 
 
 @app.get("/posts/{id}")
 def get_one_post(id: int, response: Response):
-    post = find_post(id)
+    cursor.execute("""SELECT * FROM posts WHERE id = %s """, str(id))
+    post = cursor.fetchone()
 
     if not post:
         raise HTTPException(
@@ -92,37 +65,43 @@ def get_one_post(id: int, response: Response):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def post_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"detail": post_dict}
+    cursor.execute(
+        """INSERT INTO Posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+        (post.title, post.content, post.published)
+        )
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"detail": new_post}
 
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *  """, str(id))
+    deleted_post = cursor.fetchone()
+    conn.commit()
 
-    if index == None:
+    if deleted_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id {id} does not exist."
             )
     
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
+    cursor.execute(
+        """UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+        (post.title, post.content, post.published, str(id))
+        )
+    updated_post = cursor.fetchone()
+    conn.commit()
 
-    if index == None:
+    if updated_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id {id} does not exist."
             )
     
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-    return {"detail": post_dict}
+    return {"detail": updated_post}
